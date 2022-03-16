8e8b5496c30c9159d6610311722d7fa0c6df4b07
Udosinachi <udosipat@gmail.com>
2022-03-16 17:27:22 +0100

    Update Style.js

--- src/components/Carrousel/Style.js
+++ src/components/Carrousel/Style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Caro = styled.div`
-  border: 3px solid red;
+  /* border: 3px solid red; */
   height: 100vh;
   background-image: url('/images/oma3.jpg');
   background-repeat: no-repeat;
@@ -10,23 +10,35 @@ export const Caro = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: rgba(255, 255, 255, 0.45);
+  background-color: rgba(255, 255, 255, 0.75);
   background-blend-mode: overlay;
+  @media (max-width: 1027px) {
+    height: 60vh;
+  }
   .caro-text-div {
-    border: 3px solid red;
-    width: 70%;
+    /* border: 3px solid red; */
+    width: 1000px;
     margin: auto;
+    @media (max-width: 1027px) {
+      width: 90%;
+    }
     div {
       font-size: 40px;
+      @media (max-width: 1000px) {
+        font-size: 25px;
+      }
     }
     p {
       font-size: large;
+      @media (max-width: 1000px) {
+        font-size: medium;
+      }
     }
     button {
       border: 4px solid black;
       background: none;
       width: 200px;
-      height: 40px;
+      height: 50px;
       font-size: medium;
       margin-top: 20px;
     }
