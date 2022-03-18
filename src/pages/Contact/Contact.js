bcbff8bec3c3cf5bb2b710425563bdd5dc389bad
Udosinachi <udosipat@gmail.com>
2022-03-18 20:44:37 +0100

    Ishi ji

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -16,6 +16,42 @@ const Contact = () => {
           <ChevronRightIcon className='icon' />
           <span>CONTACT</span>
         </div>
+
+        <div className='heading'>
+          <div>
+            <strong>MY</strong> LOCATION
+          </div>
+          <p>VISIT THE BEST HAIRDRESSING SALON IN CALABAR</p>
+          <span>425W. 14th Street, 2nd FloorNew York, New York 10014</span>
+        </div>
+
+        <div className='contact-form'>
+          <div className='form-head'>
+            <div>
+              <strong>CONTACT ME</strong> TODAY
+            </div>
+            <p>
+              GET IN TOUCH WITH ME REGARDING ANY QUESTIONS ON OUR SERVICES OR
+              SIMPLY TO BOOK AN APPOINTMENT!
+            </p>
+          </div>
+
+          <form>
+            <label>
+              Your Name:
+              <input type='text' />
+            </label>
+            <label>
+              Your Email:
+              <input type='text' />
+            </label>
+            <label>
+              Your Review:
+              <input type='textarea' />
+            </label>
+            <button>Submit</button>
+          </form>
+        </div>
       </Con>
     </div>
   )
--- src/pages/Contact/Style.js
+++ src/pages/Contact/Style.js
@@ -21,4 +21,69 @@ export const Con = styled.div`
       font-weight: 600;
     }
   }
+  .heading {
+    /* border: 3px solid red; */
+    text-align: center;
+    width: 90%;
+    margin: 50px auto 30px auto;
+    div {
+      font-size: xx-large;
+      @media (max-width: 700px) {
+        font-size: xx-large;
+        padding: 0;
+      }
+    }
+    p {
+      font-size: x-large;
+      @media (max-width: 700px) {
+        padding: 0;
+        margin: 10px 0;
+        font-size: x-large;
+      }
+      @media (max-width: 500px) {
+        font-size: large;
+      }
+    }
+    span {
+      @media (max-width: 700px) {
+        font-size: medium;
+      }
+      @media (max-width: 500px) {
+        font-size: 14px;
+      }
+    }
+  }
+
+  .contact-form {
+    border: 3px solid red;
+    background-color: silver;
+    .form-head {
+      text-align: center;
+      border: 3px solid green;
+      width: 50%;
+      margin: 30px auto;
+      @media (max-width: 1000px) {
+        width: 95%;
+      }
+      div {
+        font-size: xx-large;
+      }
+      p {
+        font-size: large;
+        opacity: 0.6;
+      }
+    }
+    form {
+      border: 3px solid blue;
+      display: flex;
+      flex-direction: column;
+      label {
+        display: flex;
+        flex-direction: column;
+        input {
+          height: 45px;
+        }
+      }
+    }
+  }
 `
